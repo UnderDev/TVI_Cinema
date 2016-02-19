64e559a8434a8e251fd5fe1cbf4eae558f5b3f32
Yvonne <yvonnegrealy@hotmail.com>
2016-02-19 21:37:19 +0000

    ImageSlider
    Can't get it to work from external js file but runs from inline
    javascript on masterpage. Have included ImageSlider.js file anyway

---
+++ scripts/ImageSlider.js
@@ -0,0 +1,13 @@
+﻿
+
+    
+    function fun() {
+        i++;
+        document.getElementById("img1").src = "Images/" + i + ".png";
+        if (i == 2) //here 2 is number of images i want to display in the slide show
+        { i = 0; }
+
+        return; 
+    }
+    
+
