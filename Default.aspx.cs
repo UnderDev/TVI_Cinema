9800afab1dea7b86854fe155fe8e282bbe9915f6
Yvonne <yvonnegrealy@hotmail.com>
2016-04-10 10:55:54 +0100

    Home page changes

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,8 +7,28 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+
+    }
+
+    public string GetImage(object img)
+    {
+        return "data:image/jpg;base64," + Convert.ToBase64String((byte[])img);
+    }
+
+
+    //when photo is clicked, event object should set statesession="name_of_movie" and send to movie page
+   
+    
+    protected void DataList2_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        
+        DataList2.SelectedIndex = e.Item.ItemIndex;
+        Label1.Text = ((Label)DataList2.SelectedItem.FindControl("NameLabel")).Text.ToString();
+        
     }
 }
