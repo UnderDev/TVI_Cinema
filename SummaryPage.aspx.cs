748975975a8756a9c5e78e2da9db8eba4e293b67
arnoldout <arnoldout@gmail.com>
2016-04-12 09:16:32 +0100

    Added Booking Stuff
    Added 3 booking pages, altered the scope of the genDates method in
    master to public. Need to convert total from currency

---
+++ SummaryPage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SummaryPage : System.Web.UI.Page
+{
+    public Booking booking;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if ((Booking)Session["booking"] != null)
+        {
+            booking = (Booking)Session["booking"];
+        }
+        else
+        {
+            booking = new Booking(2, DateTime.Now, 2);
+            //put back in when ready to push
+            //Response.Redirect("BookingPage.aspx");
+        }
+    }
+}
